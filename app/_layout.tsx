68c6fb254e0c5539e6933bea26f4bdaca5d162ce
Daniel <156960927+U22099@users.noreply.github.com>
2026-01-13 23:33:44 +0100

    feat: removed removal of status bar code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,11 @@
 import { Stack } from "expo-router";
 import "./globals.css";
-import { StatusBar } from "react-native";
+// import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
     <>
-      <StatusBar hidden={true} />
+      {/* <StatusBar hidden={true} /> */}
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
